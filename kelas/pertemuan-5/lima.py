c2533478e0c93bf23771587e6e54de5995952a37
extrayiban <mzidanabdulkadir@gmail.com>
2025-10-09 14:20:03 +0800

    pertemuan5

---
+++ kelas/pertemuan-5/lima.py
@@ -0,0 +1,19 @@
+praktikum = ["APD","Orsikom","jarkom",["RPL","basis data","struktur data"]]
+
+praktikum.remove("APD") #
+# praktikum.append("Basdat")
+# praktikum.insert(1,"basdat")
+buang = praktikum.pop() # maling anak list praktikum terus di simpan
+# praktikum[2] = "Kalkulus"
+
+for i in praktikum:
+    print(i)
+
+# print(praktikum[3][1])
+
+angka = [1,2,3,4,5,6,7,8,9]
+huruf = ["a","b","c"]
+kombinasi = angka + huruf
+print(kombinasi)
+
+tuple = ('apd',"")
