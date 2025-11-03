14bbe613cdad39a9f6636df67af55b1fb90d8fec
extrayiban <mzidanabdulkadir@gmail.com>
2025-11-03 20:00:16 +0800

    PT8

--- post-test/post-test-apd-8/utils/warna.py
+++ post-test/post-test-apd-8/utils/warna.py
@@ -1,7 +1,6 @@
 from colorama import Fore, Style, init
 init(autoreset=True)
 
-# Warna yang umum dipakai
 TITLE = Fore.CYAN + Style.BRIGHT
 SUCCESS = Fore.GREEN + Style.BRIGHT
 ERROR = Fore.RED + Style.BRIGHT
